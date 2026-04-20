699a0e7c263ee1b375469553f455347bf1cdb079
heliobrandao <heliob79@gmail.com>
2026-04-20 17:17:21 -0300

    Replace incorrect project with spring notification api

---
+++ src/main/java/com/damageinc/notification/SpringNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.damageinc.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNotificationApiApplication.class, args);
+	}
+
+}

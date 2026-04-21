ce66d87b9e74b7b8f7a7d3982b0ed52af622fca5
heliobrandao <heliob79@gmail.com>
2026-04-21 07:40:02 -0300

    Add controller tests with MockMvc

---
+++ src/test/java/com/damageinc/notification/controller/NotificationControllerTest.java
@@ -0,0 +1,48 @@
+package com.damageinc.notification.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.damageinc.notification.factory.NotificationFactory;
+import com.damageinc.notification.service.NotificationService;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(NotificationController.class)
+class NotificationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private NotificationFactory factory;
+
+    @Test
+    void shouldSendNotificationSuccessfully() throws Exception {
+
+        NotificationService service = mock(NotificationService.class);
+
+        when(factory.getNotificationService("email"))
+                .thenReturn(service);
+
+        String json = """
+        {
+          "type":"email",
+          "message":"hello"
+        }
+        """;
+
+        mockMvc.perform(post("/notify")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status")
+                        .value("success"));
+    }
+}
